1fbaeb1e8e81047c461c2c2d89c2cb483762e3fe
Daniel1124-gif <SDSO112024@gmail.com>
2024-11-25 12:29:23 +0100

    Add files via upload

---
+++ Oppgave If Else statment/script.js
@@ -0,0 +1,11 @@
+let userName = "John";
+let userAge = 20;
+let userCity = "Bergen";
+
+if (userAge > 18 && userCity === "Bergen") {
+    console.log("Du er over 18, velkommen in"); 
+ } else
+ {console.log("Du er ikke gammel nok, og du er fra feil by");
+    } 
+  if (userName === "John" && userCity === "Bergen") ;
+ {console.log("Velkommen inn, John fra Bergen!");}
